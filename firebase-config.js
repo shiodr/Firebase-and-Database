00c38f216258f61b2aa6650275c94cf32c8b2297
shiodr <ShioriTP2286@gmail.com>
2026-08-21 10:27:27 +0800

    feat: update Firebase configuration with project-specific credentials and enable analytics

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,20 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyBkaQyxobwqVIqrI7pFSu2Mi_GZad5jh04",
+  authDomain: "studentrm-cfbf3.firebaseapp.com",
+  projectId: "studentrm-cfbf3",
+  storageBucket: "studentrm-cfbf3.firebasestorage.app",
+  messagingSenderId: "775126945845",
+  appId: "1:775126945845:web:be3ba478198356e23f217c",
+  measurementId: "G-112H6EN9B7"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
